5b286543e7f1456f74ed292f74c42da13cac6ff4
MazenYasser1 <160043731+MazenYasser1@users.noreply.github.com>
2024-02-18 18:19:52 +0200

    Add files via upload

--- ZelzalMusic/plugins/tools/queue.py
+++ ZelzalMusic/plugins/tools/queue.py
@@ -170,17 +170,17 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
     for x in got:
         j += 1
         if j == 1:
-            msg += f'جـارِ التشغيـل الآن :\n\n✧العنوان : {x["title"]}\nالمدة : {x["dur"]}\nبواسطة : {x["by"]}\n\n'
+            msg += f'جـارِ التشغيـل حاليـاً :\n\n🏷العنوان : {x["title"]}\nالمدة : {x["dur"]}\nبواسطة : {x["by"]}\n\n'
         elif j == 2:
-            msg += f'فـي الانتظـار :\n\n✧العنوان : {x["title"]}\nالمدة : {x["dur"]}\nبواسطة : {x["by"]}\n\n'
+            msg += f'فـي الانتظـار :\n\n🏷العنوان : {x["title"]}\nالمدة : {x["dur"]}\nبواسطة : {x["by"]}\n\n'
         else:
-            msg += f'✧العنوان : {x["title"]}\nالمدة : {x["dur"]}\nبواسطة : {x["by"]}\n\n'
+            msg += f'🏷العنوان : {x["title"]}\nالمدة : {x["dur"]}\nبواسطة : {x["by"]}\n\n'
     if "Queued" in msg:
         if len(msg) < 700:
             await asyncio.sleep(1)
             return await CallbackQuery.edit_message_text(msg, reply_markup=buttons)
-        if "✧" in msg:
-            msg = msg.replace("✧", "")
+        if "🏷" in msg:
+            msg = msg.replace("🏷", "")
         link = await ZelzalyBin(msg)
         med = InputMediaPhoto(media=link, caption=_["queue_3"].format(link))
         await CallbackQuery.edit_message_media(media=med, reply_markup=buttons)
